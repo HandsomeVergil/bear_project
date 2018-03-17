683cc89ff2fb4392e5b7ca45355a06a9c0151cbf
HandsomeVergil <raymanxray08@yandex.ru>
2018-03-17 17:54:56 +0300

    Ввел координаты(Не работают)

--- bear_project.py
+++ bear_project.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    result=requests.get('https://search-maps.yandex.ru/v1/',params={'apikey':'4ad3e4de-10fa-4349-b95f-d4bf6e5524c2','text':'Пивная','lang':'ru_RU'})
+    result=requests.get('https://search-maps.yandex.ru/v1/',params={'apikey':'4ad3e4de-10fa-4349-b95f-d4bf6e5524c2','text':'Пивная','lang':'ru_RU','ll':'55.4507,37.3656','spn':'spn:0.552069,0.400552'})
     print(result.json())
     return jsonify(result.json())
 if __name__ =='__main__':
