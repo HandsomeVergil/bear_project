5d8c538607260d25923b5e8545045b416d0238de
HandsomeVergil <raymanxray08@yandex.ru>
2018-03-17 17:01:27 +0300

    Добавлен локальный сайт со словарями геоданных

--- bear_project.py
+++ bear_project.py
@@ -1,7 +1,13 @@
-def get_func(chislo):
-    print(a)
-    c=a+b
-    print(c)
-a=21
-b=13
-get_func(a)
+from flask import Flask,jsonify
+import requests
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    result=requests.get('https://search-maps.yandex.ru/v1/',params={'apikey':'4ad3e4de-10fa-4349-b95f-d4bf6e5524c2','text':'Пивная','lang':'ru_RU'})
+    print(result.json())
+    return jsonify(result.json())
+if __name__ =='__main__':
+    app.run()
+index()
