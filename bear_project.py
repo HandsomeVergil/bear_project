5a3079aedfb56fa811b31faa6074c07d7c028faf
HandsomeVergil <raymanxray08@yandex.ru>
2018-03-24 16:21:45 +0300

    Добавлен сервер и карта

--- bear_project.py
+++ bear_project.py
@@ -1,13 +1,14 @@
-from flask import Flask,jsonify
-import requests
+from flask import Flask, render_template, request 
 
-app=Flask(__name__)
 
-@app.route('/')
+my_flask_app=Flask(__name__)
+
+@my_flask_app.route('/')
 def index():
-    result=requests.get('https://search-maps.yandex.ru/v1/',params={'apikey':'4ad3e4de-10fa-4349-b95f-d4bf6e5524c2','text':'Пивная','lang':'ru_RU','ll':'55.4507,37.3656','spn':'0.552069,0.400552'})
-    print(result.json())
-    return jsonify(result.json())
-if __name__ =='__main__':
-    app.run()
-index()
+    return render_template('index1.html')
+
+
+if __name__=='__main__':
+    my_flask_app.run(debug=True)
+ 
+
