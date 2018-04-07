e102894f25dade25b2a321c421aed6ed02219088
HandsomeVergil <raymanxray08@yandex.ru>
2018-04-07 18:44:39 +0300

    Добавлены метки на карту

---
+++ Static/js/object_manager.js
@@ -0,0 +1,53 @@
+ymaps.ready(init);
+
+function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.76, 37.64],
+            zoom: 10
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        objectManager = new ymaps.ObjectManager({
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+            clusterize: true,
+            // ObjectManager принимает те же опции, что и кластеризатор.
+            gridSize: 32,
+            clusterDisableClickZoom: true
+        });
+    var geolocation = ymaps.geolocation;
+
+
+    // Чтобы задать опции одиночным объектам и кластерам,
+    // обратимся к дочерним коллекциям ObjectManager.
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+    myMap.geoObjects.add(objectManager);
+
+    $.ajax({
+        url: "main"
+    }).done(function(data) {
+        objectManager.add(data);
+    });
+
+    geolocation.get({
+        provider: 'yandex',
+        mapStateAutoApply: true
+    }).then(function (result) {
+        // Красным цветом пометим положение, вычисленное через ip.
+        result.geoObjects.options.set('preset', 'islands#redCircleIcon');
+        result.geoObjects.get(0).properties.set({
+            balloonContentBody: 'Мое местоположение'
+        });
+        myMap.geoObjects.add(result.geoObjects);
+    });
+
+    geolocation.get({
+        provider: 'browser',
+        mapStateAutoApply: true
+    }).then(function (result) {
+        // Синим цветом пометим положение, полученное через браузер.
+        // Если браузер не поддерживает эту функциональность, метка не будет добавлена на карту.
+        result.geoObjects.options.set('preset', 'islands#blueCircleIcon');
+        myMap.geoObjects.add(result.geoObjects);
+    });
+}
--- bear_project.py
+++ bear_project.py
@@ -9,15 +9,45 @@ def index():
     return render_template('index1.html')
 @my_flask_app.route('/main')
 def api_acces():
-    result=requests.get('https://search-maps.yandex.ru/v1/',params={'apikey':'4ad3e4de-10fa-4349-b95f-d4bf6e5524c2','text':'Пивная','lang':'ru_RU','ll':'37.618920,55.756994','spn':'0.552069,0.400552'})
+    result=requests.get('https://search-maps.yandex.ru/v1/',params={'apikey':'4ad3e4de-10fa-4349-b95f-d4bf6e5524c2','text':'Пивная','lang':'ru_RU',
+    	'bbox':'36.83,55.67~38.24,55.91',
+    	'results':'420'
+    	#'ll':'37.618920,55.756994',
+    	#'spn':'0.552069,0.400552'
+    	})
     #print(result.text.encode('utf-8'))
-    for value in result.json()['features']:
-    	properties=value['properties']
-    	print(properties['name'])
-    return jsonify(result.json())
+    d_result=result.json()
+    features=[]
+    _id=0
+    my_list=[]
+    for geom in d_result['features']:
+    	geometry=geom['geometry']
+    	coordinates=geometry['coordinates']
+    	x,y=coordinates
+    	#print(x,',',y)
+    	name=geom['properties']['name']
+    	my_list.append({'name':name,'x':x,'y':y})
+    	features.append( {
+        "type": "Feature", 
+        "id": _id, 
+        "geometry": {"type": "Point", "coordinates": [y,x]},
+         "properties": {"clusterCaption": "<strong> %s </strong>" %name}
+         })
+    	print(name,x,y)
+    	_id+=1
+
+    #return render_template('coor_name.html', my_list=my_list)
+    #return jsonify(d_result)
+    return jsonify({
+    "type": "FeatureCollection",
+    "features": features})
+@my_flask_app.route('/coor_name')
+def coor_name():
+    return render_template('coor_name.html' )
 
 
 if __name__=='__main__':
-    my_flask_app.run()
+    my_flask_app.run(debug=True)
  
+
 
